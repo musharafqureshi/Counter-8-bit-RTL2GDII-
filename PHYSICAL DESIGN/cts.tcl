c144b0ffa299d624832260d9e0f0362cb7ef688b
Musharaf_64 <musharafqureshi626@gmail.com>
2026-08-29 14:43:11 +0530

    Add clock tree synthesis configuration in cts.tcl

---
+++ PHYSICAL DESIGN/cts.tcl
@@ -0,0 +1,12 @@
+add_ndr -width {Metal1 0.12 Metal2 0.14 Metal3 0.14 Metal4 0.14 Metal5 0.14 Metal6 0.14 Metal7 0.14 Metal8 0.14 Metal9 0.14} -spacing {Metal1 0.12 Metal2 0.14 Metal3 0.14 Metal4 0.14 Metal5 0.14 Metal6 0.14 Metal7 0.14 Metal8 0.14 Metal9 0.14} -name 2w2s
+create_route_type -name clkroute -non_default_rule 2w2s -bottom_preferred_layer Metal5 -top_preferred_layer Metal6
+set_ccopt_property route_type clkroute -net_type trunk
+set_ccopt_property route_type clkroute -net_type leaf
+set_ccopt_property buffer_cells {CLKBUFX6 CLKBUFX8 CLKBUFX12}
+set_ccopt_property inverter_cells {CLKINVX6 CLKINVX8 CLKINVX12}
+set_ccopt_property clock_gating_cells TLATNTSCA*
+create_ccopt_clock_tree_spec -file ccopt_spec
+source ccopt_spec
+ccopt_design -cts 
+report_ccopt_clock_trees -file clk_tree.rpt
+report_ccopt_skew_groups -file skew_groups.rpt 
